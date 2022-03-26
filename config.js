915995bb1a67c8c67129fe874053d5ad15b157f5
china-wct <89726144+china-wct@users.noreply.github.com>
2022-03-26 15:16:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export default {
   githubName: 'china-wct',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
-  domain: 'https://blog.halberd.cn',
+  domain: 'https://wctbk.vercel.app',
   SEO_description: 'WCT博客，写我所想',
   SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,WCT博客，自由的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
