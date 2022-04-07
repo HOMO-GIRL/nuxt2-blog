d22aa720250ecf84b571596a715a8a7764e350b7
HOMO-GIRL <103185426+HOMO-GIRL@users.noreply.github.com>
2022-04-07 18:53:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  title: 'WCT博客',
-  githubName: 'china-wct',
+  title: 'HOMO-GIRL博客',
+  githubName: 'HOMO-GIRL',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
-  domain: 'https://wctbk.vercel.app',
-  SEO_description: 'WCT博客，写我所想',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,WCT博客，自由的博客',
+  domain: 'https://HOMO-GIRL.vercel.app',
+  SEO_description: 'HOMO-GIRL博客，写我所想',
+  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,HOMO-GIRL博客，自由的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
